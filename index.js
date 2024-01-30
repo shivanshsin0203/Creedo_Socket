ac09097a251cd71e5171937ad43b9ebe0791d30a
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-30 16:08:43 +0530

    Notification Added

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected " + socket.id);
+  socket.on("friend_request", (data) => {
+    console.log(data);
+    io.emit("recived_frequest", data);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+
+httpServer.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
